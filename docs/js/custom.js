ce9c5a3bd676ab0dc4e4d738f9ad816891c6f159
Altria <altriabot@gmail.com>
2022-11-01 07:26:35 +0800

    主题配置更新

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -94,3 +94,14 @@ window.onload = function() {
       }
     });
   }
+/* 拉姆蕾姆回到顶部或底部按钮 */
+$(function() {
+	$("#lamu img").eq(0).click(function() {
+		$("html,body").animate({scrollTop:$(document).height()},800);
+		return false;
+	});
+	$("#leimu img").eq(0).click(function() {
+		$("html,body").animate({scrollTop:0},800);
+		return false;
+	});
+});
